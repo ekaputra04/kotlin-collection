2c25a5504e441c2456eb26de34bbaa5509b17454
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-02-01 15:05:12 +0800

    aggregate

---
+++ src/app/Aggregate.kt
@@ -0,0 +1,12 @@
+package app
+
+fun main() {
+    val numbers = (1..10).toList()
+
+    println(numbers)
+    println(numbers.min())
+    println(numbers.max())
+    println(numbers.average())
+    println(numbers.sum())
+    println(numbers.count())
+}
