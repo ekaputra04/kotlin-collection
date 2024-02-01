442119234904601659cfe2d8e6d3b0d402663387
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-02-01 14:09:14 +0800

    windowed

---
+++ src/app/Windowed.kt
@@ -0,0 +1,16 @@
+package app
+
+fun main() {
+    val range = (1..5).toList()
+    println(range.windowed(3, 1, false))
+    println(range.windowed(3, 1, false) { it.size })
+
+    println(range.windowed(3, 1, true))
+    println(range.windowed(3, 1, true) { it.size })
+
+    println(range.windowed(3, 2, false))
+    println(range.windowed(3, 2, false) { it.size })
+
+    println(range.windowed(3, 2, true))
+    println(range.windowed(3, 2, true) { it.size })
+}
